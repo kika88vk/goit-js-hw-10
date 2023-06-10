75a6f4904fe84dfdf058d9f86f92ba2041340785
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-06-11 01:10:06 +0300

    Add fetch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import Notiflix from 'notiflix';
 import "notiflix/dist/notiflix-3.2.6.min.css";
 import SlimSelect from 'slim-select';
+import { fetchBreeds } from './cat-api';
+import { fetchCatByBreed } from './cat-api';
 
 const selectEl = document.querySelector('.breed-select');
+const infoBox = document.querySelector('.cat-info');
 
-new SlimSelect({
+
+
+fetchCatByBreed("abys").then(data => console.log(data))
+    .catch(() => { Notiflix.Notify.failure(`❌ Oops! Something went wrong! Try reloading the page!`) });
+
+const selectInput = new SlimSelect({
     select: selectEl,
+    settings: {
+        contentLocation: infoBox,
+    },
+
 })
+
+function getNameCats(id, name) {
+
+}
+
+const catsID = fetchBreeds().then(datas => {
+    const cat = datas.map(data =>
+        `<option value="${data.id}">${data.name}</option>`).join("");
+    selectEl.insertAdjacentHTML("afterbegin", cat);
+}).catch(() => { Notiflix.Notify.failure(`❌ Oops! Something went wrong! Try reloading the page!`) })
+
+
+
