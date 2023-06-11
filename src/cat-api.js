24dc9d10488e8186c3df5f7d45d84689956d137d
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-06-11 23:44:02 +0300

    Done js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,26 +1,38 @@
 
 export function fetchBreeds() {
-    const options = {
-        headers: {
-            x_api_key: 'live_t61OFD1JL2gLGVATHVICdXA3FOIP1yxmLo3WhhrUSkhtPs7LfS5YePGf8Of7HDMC',
-        },
-    };
+    // const options = {
+    //     headers: {
+    //         x_api_key: 'live_t61OFD1JL2gLGVATHVICdXA3FOIP1yxmLo3WhhrUSkhtPs7LfS5YePGf8Of7HDMC',
+    //         breedId,
+    //     },
+
+    // };
+    // const url = 'https://api.thecatapi.com/v1/breeds';
+
+    // return fetch(url, options).
+    //     then(response => {
+    //         if (!response.ok) {
+    //             throw new Error(response.statusText);
+    //         }
+    //         return response.json()
+    //     });
+
+    const x_api_key = "live_t61OFD1JL2gLGVATHVICdXA3FOIP1yxmLo3WhhrUSkhtPs7LfS5YePGf8Of7HDMC";
     const url = 'https://api.thecatapi.com/v1/breeds';
 
-    return fetch(url, options).
-        then(response => {
+    return fetch(`${url}`)
+        .then(response => {
             if (!response.ok) {
                 throw new Error(response.statusText);
             }
             return response.json()
         });
-
 };
 
 export function fetchCatByBreed(breedId) {
     const x_api_key = "live_t61OFD1JL2gLGVATHVICdXA3FOIP1yxmLo3WhhrUSkhtPs7LfS5YePGf8Of7HDMC";
     const base_url = 'https://api.thecatapi.com/v1';
-    return fetch(`${base_url}/images/search?key=${x_api_key}&breed_ids=${breedId}`)
+    return fetch(`${base_url}/images/search?breed_ids=${breedId}&api_key=${x_api_key}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.statusText);
--- src/index.js
+++ src/index.js
@@ -1,34 +1,62 @@
 import Notiflix from 'notiflix';
 import "notiflix/dist/notiflix-3.2.6.min.css";
-import SlimSelect from 'slim-select';
+// import SlimSelect from 'slim-select';
 import { fetchBreeds } from './cat-api';
 import { fetchCatByBreed } from './cat-api';
 
 const selectEl = document.querySelector('.breed-select');
 const infoBox = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
 
 
+getNameCats();
 
-fetchCatByBreed("abys").then(data => console.log(data))
-    .catch(() => { Notiflix.Notify.failure(`❌ Oops! Something went wrong! Try reloading the page!`) });
 
-const selectInput = new SlimSelect({
-    select: selectEl,
-    settings: {
-        contentLocation: infoBox,
-    },
+selectEl.addEventListener("change", onSelectBreed);
 
-})
+function onSelectBreed() {
+    infoBox.innerHTML = "";
+    infoBox.style.display = "none";
+    const breedID = selectEl.value;
+    loader.style.display = "block";
 
-function getNameCats(id, name) {
+    fetchCatByBreed(breedID).then(cats => {
+        const catEl = cats.map(cat =>
+            `<img src="${cat.url}" alt="cat">
+            <h1 class="breed-name">${cat.breeds[0].name}</h1>
+            <p class="descripyion">${cat.breeds[0].description}</p>
+            <p class="temperament"><span class="bold-temperament">Temperament: </span> ${cat.breeds[0].temperament}</p>
+            `).join("");
+        infoBox.style.display = "block";
+        infoBox.innerHTML += catEl;;
+    }).catch(() => { Notiflix.Notify.failure(`❌ Oops! Something went wrong! Try reloading the page!`) })
+        .finally(() => { loader.style.display = "none" });
 
 }
 
-const catsID = fetchBreeds().then(datas => {
-    const cat = datas.map(data =>
-        `<option value="${data.id}">${data.name}</option>`).join("");
-    selectEl.insertAdjacentHTML("afterbegin", cat);
-}).catch(() => { Notiflix.Notify.failure(`❌ Oops! Something went wrong! Try reloading the page!`) })
+
+// const selectInput = new SlimSelect({
+//     select: selectEl,
+//     settings: {
+//         contentLocation: infoBox,
+//     },
+
+// })
+
+function getNameCats() {
+    selectEl.style.display = "none";
+    loader.style.display = "block";
+
+    fetchBreeds().then(datas => {
+        const cat = datas.map(data =>
+            `<option value="${data.id}">${data.name}</option>`).join("");
+        selectEl.style.display = "block";
+        selectEl.insertAdjacentHTML("afterbegin", cat);
+    }).catch(() => { Notiflix.Notify.failure(`❌ Oops! Something went wrong! Try reloading the page!`) })
+        .finally(() => { loader.style.display = "none" });
+}
+
+
 
 
 
